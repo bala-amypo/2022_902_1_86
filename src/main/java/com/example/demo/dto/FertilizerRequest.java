e2cfa2aa437e1c74e00674328c85d0e921a25cd9
bala-amypo <bala@amypo.in>
2025-12-26 05:13:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/FertilizerRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Table(name = "fertilizers")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fertilizer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String npkRatio;
+
+    @NotBlank
+    @Column(length = 500)
+    private String recommendedForCrops;
+}
