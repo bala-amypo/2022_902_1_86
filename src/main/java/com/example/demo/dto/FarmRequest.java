c635e10bfa142ad942206c0f45d3df333946788e
bala-amypo <bala@amypo.in>
2025-12-27 06:57:38 +0000

    User commit

--- src/main/java/com/example/demo/dto/FarmRequest.java
+++ src/main/java/com/example/demo/dto/FarmRequest.java
@@ -1,10 +1,41 @@
 package com.example.demo.dto;
 
-import lombok.Data;
-
-@Data
 public class FarmRequest {
-    private Double soilPH;
-    private Double waterLevel;
-    private String season;
+
+    private String name;
+    private String location;
+    private Double area;
+
+    public FarmRequest() {
+    }
+
+    public FarmRequest(String name, String location, Double area) {
+        this.name = name;
+        this.location = location;
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
 }
