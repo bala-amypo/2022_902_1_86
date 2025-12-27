3dca85309b7f4a81b34c8d39ac9e9a60eac363a2
bala-amypo <bala@amypo.in>
2025-12-27 09:12:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/Suggestion.java
+++ src/main/java/com/example/demo/entity/Suggestion.java
@@ -1,7 +1,11 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,20 +18,20 @@ public class Suggestion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "farm_id", nullable = false)
     private Farm farm;
-    
-    @Column(nullable = false)
+
+    @Column(length = 1000)
     private String suggestedCrops;
-    
-    @Column(nullable = false)
+
+    @Column(length = 1000)
     private String suggestedFertilizers;
-    
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
-    
+
     @PrePersist
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
