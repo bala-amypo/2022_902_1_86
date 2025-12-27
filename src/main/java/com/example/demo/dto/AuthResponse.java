2d9208b1b5e25d1b43d49b2d826e3ca2fad82c71
bala-amypo <bala@amypo.in>
2025-12-27 04:52:13 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -5,8 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
+    private String email;
+    private String role;
 }
--- src/main/java/com/example/demo/dto/CropRequest.java
+++ src/main/java/com/example/demo/dto/CropRequest.java
@@ -1,8 +1,16 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CropRequest {
     private String name;
+    private Double suitablePHMin;
+    private Double suitablePHMax;
+    private Double requiredWater;
+    private String season;
 }
