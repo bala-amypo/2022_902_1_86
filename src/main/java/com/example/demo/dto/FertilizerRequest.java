987359c046163044b78455d4656f2c1babd3a0dc
bala-amypo <bala@amypo.in>
2025-12-27 04:55:14 +0000

    User commit

--- src/main/java/com/example/demo/dto/FertilizerRequest.java
+++ src/main/java/com/example/demo/dto/FertilizerRequest.java
@@ -1,8 +1,14 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FertilizerRequest {
-    private String cropName;
+    private String name;
+    private String npkRatio;
+    private String recommendedForCrops;
 }
