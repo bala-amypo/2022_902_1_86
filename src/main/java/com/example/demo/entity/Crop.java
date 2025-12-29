db692a03f5fc83007800774331d11e095e935701
bala-amypo <bala@amypo.in>
2025-12-29 08:14:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/Crop.java
+++ src/main/java/com/example/demo/entity/Crop.java
@@ -3,18 +3,11 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "crops")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Crop {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,4 +27,65 @@ public class Crop {
 
     @NotBlank
     private String season;
+
+    public Crop() {
+    }
+
+    public Crop(Long id, String name, Double suitablePHMin, Double suitablePHMax,
+                Double requiredWater, String season) {
+        this.id = id;
+        this.name = name;
+        this.suitablePHMin = suitablePHMin;
+        this.suitablePHMax = suitablePHMax;
+        this.requiredWater = requiredWater;
+        this.season = season;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getSuitablePHMin() {
+        return suitablePHMin;
+    }
+
+    public Double getSuitablePHMax() {
+        return suitablePHMax;
+    }
+
+    public Double getRequiredWater() {
+        return requiredWater;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSuitablePHMin(Double suitablePHMin) {
+        this.suitablePHMin = suitablePHMin;
+    }
+
+    public void setSuitablePHMax(Double suitablePHMax) {
+        this.suitablePHMax = suitablePHMax;
+    }
+
+    public void setRequiredWater(Double requiredWater) {
+        this.requiredWater = requiredWater;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
 }
