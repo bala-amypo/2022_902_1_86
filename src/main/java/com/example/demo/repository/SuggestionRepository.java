8a58fd1eb25c9b82d36aaa825b7fb87b29b74b42
bala-amypo <bala@amypo.in>
2025-12-29 08:07:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/SuggestionRepository.java
+++ src/main/java/com/example/demo/repository/SuggestionRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
-import com.example.demo.entity.Fertilizer;
+import com.example.demo.entity.Suggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
-public interface FertilizerRepository extends JpaRepository<Fertilizer, Long> {
-    @Query("SELECT f FROM Fertilizer f WHERE f.recommendedForCrops LIKE %:cropName%")
-    List<Fertilizer> findByCropName(String cropName);
+public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
+    List<Suggestion> findByFarmId(Long farmId);
 }
