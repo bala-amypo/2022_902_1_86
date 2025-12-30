a62d330a65dee3f6d6704b7e304d694a22d7d634
bala-amypo <bala@amypo.in>
2025-12-30 08:55:07 +0000

    User commit

--- src/main/java/com/example/demo/service/SuggestionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-
-public interface SuggestionService {
-
-    List<String> suggestCropsAndFertilizers(Double soilPH, Double waterAvailable, String season);
-}
