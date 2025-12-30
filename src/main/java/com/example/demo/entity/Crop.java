d22e05ea5bfde2527c3df0233acdae73849257ed
bala-amypo <bala@amypo.in>
2025-12-30 08:40:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/Crop.java
+++ src/main/java/com/example/demo/entity/Crop.java
@@ -1,91 +1,40 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "crops")
 public class Crop {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank
-    @Column(length = 100)
     private String name;
-
-    @NotNull
     private Double suitablePHMin;
-
-    @NotNull
     private Double suitablePHMax;
-
-    @NotNull
     private Double requiredWater;
-
-    @NotBlank
     private String season;
 
-    public Crop() {
+    public Crop() {}
+    public Crop(Long id, String name, Double suitablePHMin, Double suitablePHMax, Double requiredWater, String season) {
+        this.id = id; this.name = name; this.suitablePHMin = suitablePHMin; 
+        this.suitablePHMax = suitablePHMax; this.requiredWater = requiredWater; this.season = season;
     }
 
-    public Crop(Long id, String name, Double suitablePHMin, Double suitablePHMax,
-                Double requiredWater, String season) {
-        this.id = id;
-        this.name = name;
-        this.suitablePHMin = suitablePHMin;
-        this.suitablePHMax = suitablePHMax;
-        this.requiredWater = requiredWater;
-        this.season = season;
-    }
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public Double getSuitablePHMin() { return suitablePHMin; }
+    public Double getSuitablePHMax() { return suitablePHMax; }
+    public String getSeason() { return season; }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Double getSuitablePHMin() {
-        return suitablePHMin;
-    }
-
-    public Double getSuitablePHMax() {
-        return suitablePHMax;
-    }
-
-    public Double getRequiredWater() {
-        return requiredWater;
-    }
-
-    public String getSeason() {
-        return season;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSuitablePHMin(Double suitablePHMin) {
-        this.suitablePHMin = suitablePHMin;
-    }
-
-    public void setSuitablePHMax(Double suitablePHMax) {
-        this.suitablePHMax = suitablePHMax;
-    }
-
-    public void setRequiredWater(Double requiredWater) {
-        this.requiredWater = requiredWater;
-    }
-
-    public void setSeason(String season) {
-        this.season = season;
+    public static CropBuilder builder() { return new CropBuilder(); }
+    public static class CropBuilder {
+        private Long id; private String name; private Double suitablePHMin; private Double suitablePHMax; private Double requiredWater; private String season;
+        public CropBuilder id(Long id) { this.id = id; return this; }
+        public CropBuilder name(String name) { this.name = name; return this; }
+        public CropBuilder suitablePHMin(Double min) { this.suitablePHMin = min; return this; }
+        public CropBuilder suitablePHMax(Double max) { this.suitablePHMax = max; return this; }
+        public CropBuilder requiredWater(Double water) { this.requiredWater = water; return this; }
+        public CropBuilder season(String season) { this.season = season; return this; }
+        public Crop build() { return new Crop(id, name, suitablePHMin, suitablePHMax, requiredWater, season); }
     }
 }
